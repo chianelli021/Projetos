0486ce36fdb8f46cd4ba8a5582b4be3696b7721d
chianelli021 <leonardochianelli1@gmail.com>
2026-04-15 09:45:30 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+#Calculadora
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+operacao = input("Digite a operação: ")
+match operacao:
+    case "+":
+        res = num1 + num2
+    case "-":
+        res = num1 - num2
+    case "*":
+        res = num1 * num2
+    case "/":
+        res = num1 / num2
+
+print(f"Resultado: {res}")
+
